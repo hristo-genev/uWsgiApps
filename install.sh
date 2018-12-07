edf4c7b589405e4037fcab5f18478607b7109fa2
HarryGG <harrygg@gmail.com>
2018-12-07 17:26:24 +0200

    working new repo

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+cwd=${PWD}
+
+echo "Creating virtual environment in ~/Env/uWsgiApps"
+mkdir ${cwd}/../Env/
+cd ${cwd}/../Env/
+virtualenv -p python3 uWsgiApps
+
+echo "Enabling virtualenv uWsgiApps"
+source ./uWsgiApps/bin/activate
+
+echo "Installing modules from requirements.txt"
+cat epgapp/requirements.txt
+
+cd ${cwd}
+pip3 install -r epgapp/requirements.txt
+
+echo "**************************************************"
 
 echo "Enabling nginx site"
 link=${PWD}/nginx/epgapp.kodibg.org
@@ -9,3 +26,8 @@ echo "Enabling uwsgi service"
 link=${PWD}/service/uwsgi.service
 sudo ln -si $link /etc/systemd/system/
 echo `ls -la /etc/systemd/system/uwsgi.service`
+
+
+echo "**************************************************"
+echo "To start the service run sudo service uwsgi start"
+echo "To see the service status run sudo service uwsgi status"
